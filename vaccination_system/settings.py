dc8f1a24f758bda859cb16e2f90c8af4e450f0bc
dabananda <imdmitra@gmail.com>
2025-04-14 14:16:41 +0600

    Dev test deploy

--- vaccination_system/settings.py
+++ vaccination_system/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app", '127.0.0.1']
+ALLOWED_HOSTS = [".vercel.app", ".now.sh", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'vaccination_system.wsgi.app'
+WSGI_APPLICATION = 'vaccination_system.wsgi.application'
 
 
 # Database
